009e2b86754475de684c8df6d0b17030fc8b7bda
clever <2283412339@qq.com>
2018-11-04 12:37:42 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class Test {
+
+        @RequestMapping("/hello")
+        public String index() {
+            return "hello";
+        }
+}
